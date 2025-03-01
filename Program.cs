f6bd998c7493577241fc14f1e0cb7d6b3c5ef746
stephenChinag <stephenchinag@gmail.com>
2025-03-01 02:51:13 +0100

    main

--- Program.cs
+++ Program.cs
@@ -1,12 +1,38 @@
-﻿namespace ConsoleApp1 {
-    internal class Program {
+﻿using System;
 
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        // Method to calculate factorial
+        public static double Factorial(int number)
+        {
+            if (number == 0)
+            {
+                return 1; // Base case: 0! = 1
+            }
+            else
+            {
+                return number * Factorial(number - 1); // Recursive case
+            }
+        }
 
+        static void Main(string[]  args)
+        {
+            Console.WriteLine("Enter a number:");
+            string input = Console.ReadLine();
+ 
+            // Input validation
+            if (int.TryParse(input, out int n) && n >= 0)
+            {
+                Console.WriteLine("Factorial of " + n + " is " + Factorial(n));
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a non-negative integer.");
+            }
 
-
-        
-        static void Main (string[] args){
-            Console.WriteLine("Hello World");
+            Console.ReadKey();
         }
     }
 }
--- obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1bcc21ee98d60731fabfb86fe6b4f7b14e86c28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e523383256e625edb325535cdd8a349d2f36976")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
